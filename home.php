47de7d84dcf948cb4eca6b03db66a3f70de73654
MorganPendragon <vincentagustin123@gmail.com>
2022-10-12 11:28:36 +0800

    home

---
+++ home.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar" style="background-color: #e3f2fd;">
+        <div class="container-fluid">
+            <form>
+                <div class="d-flex gap-2 col-6 mx-auto">
+                    <a href="#" class="btn btn-outline-success" role="button">Create</a>
+                    <a href="#link" class="btn btn-outline-primary" role="button">Read</a>
+                    <a href="#link" class="btn btn-outline-success" role="button">Update</a>
+                    <a href="#link" class="btn btn-outline-danger" role="button">Delete</a>
+                </div>
+
+            </form>
+            <form class="search d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
