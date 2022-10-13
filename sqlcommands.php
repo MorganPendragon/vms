d8078d707b1cc0a609df6dc2b8187bb2ef3c34aa
mock1ngj <randomizedgg9@gmail.com>
2022-10-13 16:49:29 +0800

    message: WIP     message:    with data     message: create modal forms still missing

--- sqlcommands.php
+++ sqlcommands.php
@@ -1,10 +1,22 @@
 <?php
 
+    function insertInto($table)
+    {
+        
+    }
+
     function showTable($table)
     {
         return "SELECT * FROM $table";
     }
+
 	function deleteRow($table, $col, $id)
 	{
 		return "DELETE FROM $table WHERE $col = $id";
 	}
+
+    function updateRow()
+    {
+
+    }
+?>
