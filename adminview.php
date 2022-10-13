cefc4f240a8f8eca50ddda217138c34947c1450e
mock1ngj <randomizedgg9@gmail.com>
2022-10-13 18:04:44 +0800

    create modal done, datepicker missing

--- adminview.php
+++ adminview.php
@@ -19,15 +19,40 @@
 
             <!-- Modal -->
             <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-dialog modal-dialog-centered modal-lg">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="createModalLabel">Create</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
-                            
-                        </div>
+                        <form>
+                            <div class="modal-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="First name">
+                                    </div>
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="Middle name">
+                                    </div>
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="Last name">
+                                    </div>
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="Suffix">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <input name="email" type="email" class="form-control" id="emailFormControl" placeholder="Email">
+                                    </div>
+                                    <div class="col">
+                                        <input type="text" class="form-control" placeholder="Telephone No.">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <!--calendar picker here-->
+                                </div>
+                        </form>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                             <button type="button" class="btn btn-primary">Save</button>
@@ -105,7 +130,6 @@
                                     </div>
                                     <!--put content here-->
                                     <div class="modal-body">
-                                        ...
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
