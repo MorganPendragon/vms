ed5a971e0f01bf4cacec5d2e5f13ccfb471cb099
mock1ngj <randomizedgg9@gmail.com>
2022-10-25 03:26:28 +0800

    reset on second click

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    var yearFilter = '';
+    var brandFilter ='';
     //caches selected tab before the page reload and shows the tab on reload
     $('a[data-bs-toggle="tab"]').on('show.bs.tab', function (e) {
         localStorage.setItem('activeTab', $(e.target).attr('href'));
@@ -17,13 +19,23 @@ $(document).ready(function () {
     });
 
     //year level filter
-    $("#yearFilter").on("change", function() {
-        var value = $(this).val();
-        console.log(value);
-
-        $("#studentContent tr").filter(function () {
-            $(this).toggle($(this).text().indexOf(value) > -1)
-        });
+    $("#yearLevelFilter option").on("click", function () {
+        var value;
+        if ($(this).val() != yearFilter) {
+            value = $(this).val();
+            $("#studentContent tr").filter(function () {
+                $(this).toggle($(this).text().indexOf(value) > -1)
+            });
+        }
+        else
+        {
+            value = '';
+            $("#studentContent tr").filter(function () {
+                $(this).toggle($(this).text().indexOf(value) > -1)
+            });
+            $('#yearFilter').prop('selectedIndex',0);
+        }
+        yearFilter = $(this).val();
     });
 
     //generate random id for student
