8e01b5cde5b19c53346418fdfa11b16ff5b97131
mock1ngj <randomizedgg9@gmail.com>
2022-10-25 01:20:16 +0800

    separate jquery

--- script.js
+++ script.js
@@ -15,4 +15,22 @@ $(document).ready(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
+
+    //year level filter
+    $("#yearFilter").on("change", function() {
+        var value = $(this).val();
+        console.log(value);
+
+        $("#studentContent tr").filter(function () {
+            $(this).toggle($(this).text().indexOf(value) > -1)
+        });
+    });
+
+    //generate random id for student
+    $("#studentForm").submit(function () {
+        var id = new Date().getFullYear().toString().substring(2);
+        id += "-";
+        id += Math.random().toString(9).substring(2, 8);
+        $("#studentID").val(id);
+    });
 });
