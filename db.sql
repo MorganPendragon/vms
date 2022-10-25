2b0208765d180825ac2880eb2a022233a2e7a5dd
mock1ngj <randomizedgg9@gmail.com>
2022-10-25 19:06:44 +0800

    add column

--- db.sql
+++ db.sql
@@ -58,7 +58,9 @@ DROP TABLE IF EXISTS `student`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `student` (
   `id` varchar(30) NOT NULL COMMENT 'Primary Key',
-  `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` mediumtext NOT NULL,
+  `yearLevel` varchar(30) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
   `email` mediumtext,
   `tel` varchar(255) DEFAULT NULL,
   `gender` varchar(50) DEFAULT NULL,
@@ -79,7 +81,7 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES ('2','test2 test2 test2','test@gmail.com','test','Male','2001-01-01','test','2001-01-01','2001-01-01',NULL),('3','test3 test3 test3','.exclamationtrueisequaltofalse@gmail.com','124','Female','2001-01-01','reee','2001-01-01','2001-01-01',NULL),('5','test5 test5 test5','randomizedgg9@gmail.com','555','Female','2000-10-04','555','2001-01-01','2001-01-01',NULL),('6','test6 tset6 test6','randomizedgg9@gmail.com','666','Female','2022-10-12','111','2001-01-01','2001-01-01',NULL),('7','test7 test7 test7','randomizedgg9@gmail.com','777','Male','2022-10-19','777','2001-01-01','2001-01-01',NULL),('ea','fname mname lname','test1@gmail.com','tel','Female','2017-07-16','add','2019-07-16',NULL,'meh'),('id','fname mname lname','test2@gmail.cpom','435','Female','2016-07-14','add','2001-01-01','2001-01-01',NULL),('reeee','fname mname lname','test1@gmail.com','tel','Female','2020-07-15','add','2001-01-01','2001-01-01',NULL),('reeeese','fname mname lname','test1@gmail.com','tel','Female','2019-08-15','add','2001-01-01','2001-01-01',NULL),('te','fname mname lname','test1@gmail.com','tel','Female','2019-07-15','add','2022-10-15',NULL,NULL),('test1','fname mname lname','test2@gmail.cpom','435','Female','2018-07-15','123',NULL,'2001-01-01',NULL),('test2','fname mname lname','test1@gmail.com','tel','Female','2013-07-14','add','2021-08-16','2020-07-16',NULL),('test4','fname mname lname','test1@gmail.com','tel','Female','2017-07-14','add',NULL,'2020-07-15',NULL),('x','fname mname lname','test1@gmail.com','tel','Male','2017-08-15','add',NULL,NULL,NULL),('y','fname mname lname','test1@gmail.com','tel','Female','2019-08-17','add','2019-08-16',NULL,'brimdaddy'),('z','fname mname lname','test1@gmail.com','tel','Female','2019-08-17','add','2019-08-16',NULL,'brimdaddy');
+INSERT INTO `student` VALUES ('3','test3 test3 test3','Grade 7','Enrolled','.exclamationtrueisequaltofalse@gmail.com','124','Female','2001-01-01','reee','2001-01-01','2001-01-01','test'),('5','test5 test5 test5','Grade 8','Enrolled','randomizedgg9@gmail.com','555','Female','2000-10-04','555','2001-01-01','2001-01-01','brimdaddy'),('6','test6 tset6 test6','Grade 8','Dropped','randomizedgg9@gmail.com','666','Female','2022-10-12','111','2001-01-01','2001-01-01','astra'),('7','test7 test7 test7','Grade 10','Enrolled','randomizedgg9@gmail.com','777','Male','2022-10-19','777','2001-01-01','2001-01-01','brimdaddy'),('id','fname mname lname','Grade 7','Enrolled','test2@gmail.cpom','435','Female','2016-07-14','add','2001-01-01','2001-01-01','astra'),('reeee','fname mname lname','Grade 7','Dropped','test1@gmail.com','tel','Female','2020-07-15','add','2001-01-01','2001-01-01','meh'),('reeeese','fname mname lname','Grade 8','Enrolled','test1@gmail.com','tel','Female','2019-08-15','add','2001-01-01','2001-01-01','brimdaddy');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -115,4 +117,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-20 14:19:57
+-- Dump completed on 2022-10-25 19:06:17
