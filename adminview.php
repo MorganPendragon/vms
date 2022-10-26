a2720fa1efb591bc5bfca3aafcd93eef35771b01
MorganPendragon <vincentagustin123@gmail.com>
2022-10-26 21:23:17 +0800

    icon

--- adminview.php
+++ adminview.php
@@ -20,6 +20,7 @@
     <!--Table Sorter plugin for jQuery-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js" integrity="sha512-qzgd5cYSZcosqpzpn7zF2ZId8f/8CHmFKZ8j7mU4OUXTNRd5g+ZHBPsgKEwoqxCtdQvExE5LprwwPAgoicguNg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="script.js"></script>
+    <script src="https://kit.fontawesome.com/9dd1cea6a6.js" crossorigin="anonymous"></script>
     <style>
         th {
             cursor: pointer;
@@ -51,13 +52,13 @@
                         <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                             <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="tableTab">
                                 <li class="nav-item active">
-                                    <a href="#student" class="nav-link active" data-bs-toggle="tab">Student</a>
+                                    <a href="#student" class="nav-link active" data-bs-toggle="tab"><i class="fa-sharp fa-solid fa-graduation-cap"></i>&nbsp;&nbsp;Student</a>
                                 </li>
                                 <li class="nav-item active">
-                                    <a href="#faculty" class="nav-link" data-bs-toggle="tab">Faculty</a>
+                                    <a href="#faculty" class="nav-link" data-bs-toggle="tab"><i class="fa-sharp fa-solid fa-user-tie"></i>&nbsp;&nbsp;Faculty</a>
                                 </li>
                                 <li class="nav-item active">
-                                    <a href="#vaccine" class="nav-link" data-bs-toggle="tab">Vaccine</a>
+                                    <a href="#vaccine" class="nav-link" data-bs-toggle="tab"><i class="fa-solid fa-syringe"></i>&nbsp;&nbsp;Vaccine</a>
                                 </li>
                             </ul>
                             <div class="pb-4 justify-content-around">
