50ba09f3633e2c9291a0eba06beb96a9983fa0dd
mock1ngj <randomizedgg9@gmail.com>
2022-11-12 21:47:51 +0800

    do not touch this

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ $(function () {
         switch ($(this).data('form')) {
             //student submit
             case 1:
-                $name = $('input[name="fname[0]"], input[name="mname[0]"], input[name="lname[0]"], input[name = "firstdoctor[0]"], input[name = "seconddoctor[0]"], input[name = "boosterdoctor[0]"]');
+                $name = $('input[name$="name[0]"], input[name$="doctor[0]"]');
                 $tel = $('input[name="tel[0]"]');
                 $finalName = $('input[name="name[0]"]');
                 //generate random id for student
@@ -103,20 +103,20 @@ $(function () {
                 break;
             //student update
             case 2:
-                $name = $('input[name="fname[1]"], input[name="mname[1]"], input[name="lname[1]"], input[name = "firstdoctor[1]"], input[name = "seconddoctor[1]"], input[name = "boosterdoctor[1]"]');
+                $name = $('input[name$="name[1]"], input[name$="doctor[1]"]');
                 $tel = $('input[name="tel[1]"]');
                 $finalName = $('input[name="name[1]"]');
                 break;
             //faculty insert
             case 3:
-                $name = $('input[name="fname[2]"], input[name="mname[2]"], input[name="lname[2]"], input[name = "firstdoctor[2]"], input[name = "seconddoctor[2]"], input[name = "boosterdoctor[2]"]');
+                $name = $('input[name$="name[2]"], input[name$="doctor[2]"]');
                 $tel = $('input[name="tel[2]"]');
                 $finalName = $('input[name="name[2]"]');
                 console.log('faculty form fired');
                 break;
             //faculty update
             case 4:
-                $name = $('input[name="fname[3]"], input[name="mname[3]"], input[name="lname[3]"], input[name = "firstdoctor[3]"], input[name = "seconddoctor[3]"], input[name = "boosterdoctor[3]"]');
+                $name = $('input[name$="name[3]"], input[name$="doctor[3]"]');
                 $tel = $('input[name="tel[3]"]');
                 $finalName = $('input[name="name[3]"]');
                 console.log('faculty update form fired');
@@ -125,34 +125,14 @@ $(function () {
                 break;
         }
         //name validation check
-        $name.each(function () {
-            if (/^$|[a-zA-Z ]$/.test($(this).val())) {
-                validated++;
-                if (validated < 4) {
-                    if (validated % 2 == 0) {
-                        formname += " " + $(this).val().toString() + " ";
-                        console.log($(this).val().toString());
-                    }
-                    else {
-                        formname += $(this).val().toString();
-                        console.log($(this).val().toString());
-                    }
-                }
-            }
-            else {
-                validated = 0;
-                $(this).siblings('p').text('invalid').fadeOut(5000);
-            }
-        });
+        console.log($(this).data('name').toString());
+        formname = $($(this).data('name')).map(function(){
+            return $(this).val();
+        }).get().join(' ');
 
         $finalName.val(formname);
         console.log(formname);
-        //tel validation
-        if (!(/^$|^09[0-9]{9,9}$/.test($tel.val()))) {
-            validated = 0;
-            $tel.siblings('p').text('invalid').fadeOut(5000);
-            console.log('invalid');
-        }
+
         validated++;
 
         //stop submit if < 5
@@ -186,21 +166,21 @@ $(function () {
     });
 
     //activate components on form
-    $date = $('input[name="firstdose[0]"], input[name="firstdose[1]"], input[name="firstdose[2]"], input[name="firstdose[3]"], input[name="seconddose[0]"], input[name="seconddose[1]"], input[name="seconddose[2]"], input[name="seconddose[3]"], input[name="booster[0]"], input[name="booster[1]"], input[name="booster[2]"], input[name="booster[3]"]');
+    $date = $('input[type="date"][name*="dose"], input[type="date"][name^="booster"]');
     $date.on('change', activateAndReq);
 
     function activateAndReq() {
-        $activate = $($(this).data('activate').toString());
-        $req = $($(this).data('required').toString());
+        console.log($(this).val().toString());
         if ($(this).val().toString()) {
-            $activate.each(function () {
+            $($(this).data('activate')).each(function () {
                 $(this).removeAttr('disabled');
             });
             $req.attr('required', 'required');
         }
         else {
-            $activate.each(function () {
+            $(this).data('required').each(function () {
                 $(this).attr('disabled', 'disabled');
+                $(this).val('');
                 $(this).removeAttr('required', 'required');
             });
         }
