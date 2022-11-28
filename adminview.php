9e2d0beb98c55d34ca3befff1848ddb6a0abc565
MorganPendragon <vincentagustin123@gmail.com>
2022-11-28 19:47:48 +0800

    button x filter

--- adminview.php
+++ adminview.php
@@ -88,6 +88,12 @@
             border-radius: 10px;
 
         }
+
+        .logout {
+            margin-top: 100%;
+            margin-left: 110px;
+            font-size: 24px;
+        }
     </style>
     <script type="text/javascript">
         $(function() {
@@ -152,18 +158,6 @@
 
 <body>
     <header>
-        <!--navbar gen rep place-->
-        <!--
-        <nav class="navbar justify-content-end px-5" style="background-color:#022e43;">
-            <ul class="nav">
-                <li><a class="btn" type="button" href="adminview.php?report=1">
-                        <span class="badge text-bg-light">GENERATE REPORTS</span>
-                        </i>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        -->
     </header>
     <main>
         <div class="row">
@@ -188,6 +182,9 @@
                                 </li>
                                 <li class="nav-item ">
                                     <button type="button" class="nav-link" data-bs-toggle="modal" data-bs-target="#reports" style="color:rgb(232, 177, 62);"><i class="bi bi-clipboard-data-fill"></i>&nbsp;&nbsp;Reports</a></button>
+                                </li>
+                                <li class="nav-item ">
+                                    <a href="login.html" class="logout nav-link" style="color:rgb(232, 177, 62);"><i class="bi bi-box-arrow-left"></i>&nbsp;&nbsp;Logout</a>
                                 </li>
                             </ul>
 
@@ -1360,15 +1357,14 @@
                                 </div>
                                 <div class="modal-body">
                                     <div class="input-group mb-5">
-                                        <select class="form-select me-2" autocomplete="off">
-                                            <option value="0" hidden>Year Level</option>b
+                                        <select class="form-select ms-2" autocomplete="off">
+                                            <option value="0" hidden>Booster Brand</option>b
                                             <option value="0">---</option>
-                                            <option value="Grade 7">Grade 7</option>
-                                            <option value="Grade 8">Grade 8</option>
-                                            <option value="Grade 9">Grade 9</option>
-                                            <option value="Grade 10">Grade 10</option>
-                                            <option value="Grade 11">Grade 11</option>
-                                            <option value="Grade 12">Grade 12</option>
+                                            <option value="astra">astra</option>
+                                            <option value="j&j ">j&j</option>
+                                            <option value="moderna ">moderna</option>
+                                            <option value="pfizer">pfizer</option>
+                                            <option value="sino">sino</option>
                                         </select>
                                         <select class="form-select ms-2" autocomplete="off">
                                             <option value="0" hidden>Vaccine Brand</option>b
@@ -1387,7 +1383,10 @@
                                         <nav class="navbar justify-content-center">
                                             <ul class="nav">
                                                 <li>
-                                                   <a href="login.html" class="btn btn-outline-success" style="color:black;">gene</a>
+                                                    <a href="login.html" class="btn btn-success" style="color:white; width:200px;">Download Report <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cloud-arrow-down" viewBox="0 0 16 16">
+                                                            <path fill-rule="evenodd" d="M7.646 10.854a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 9.293V5.5a.5.5 0 0 0-1 0v3.793L6.354 8.146a.5.5 0 1 0-.708.708l2 2z" />
+                                                            <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
+                                                        </svg></a>
                                                 </li>
                                             </ul>
                                         </nav>
